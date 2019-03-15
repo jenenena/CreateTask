136701f48c0743b34304d986a630d4e9557ad06b
Jenna Mills <superangelfish555@gmail.com>
2019-03-15 09:53:04 -0600

    Initial commit

---
+++ src/View/CreateFrame.java
@@ -0,0 +1,12 @@
+package View;
+
+import javax.swing.JFrame;
+import controller.CreateController;
+
+public class CreateFrame extends JFrame
+{
+	private CreateController appController;
+	private CreatePanel appPanel;
+	
+	public CreateFrame 
+}
